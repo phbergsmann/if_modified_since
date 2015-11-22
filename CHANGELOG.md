590a95ef997bbd71ae4af072e222cebc74cbf128
pbergsmann <p.bergsmann@opendo.at>
2015-11-22 18:04:08 +0100

    Release of new version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,12 @@
 
+VERSION 1  FIRST ALPHA RELEASE
+==============================
+
+   Version 1.0 - First alpha release
+      22/11/2015 18:04  1.0.0  initial release
+         367e48f [TASK] Adds TYPO3-specific release tasks
+         817661f [TASK] Requires phbergsmann/typo3-repository-client-rmt-bridge
+
 VERSION 0  INITIAL RELEASE
 ==========================
 
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,27 @@
 <?php
-
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'If-Modified-Since',
-	'description' => '',
-	'category' => 'plugin',
-	'author' => 'Philipp Bergsmann',
-	'author_email' => 'me@ph-bergsmann.com',
-	'state' => 'alpha',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => \PhBergsmann\IfModifiedSince\Version::VERSION,
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.4'
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'If-Modified-Since',
+  'description' => '',
+  'category' => 'plugin',
+  'author' => 'Philipp Bergsmann',
+  'author_email' => 'me@ph-bergsmann.com',
+  'state' => 'alpha',
+  'internal' => '',
+  'uploadfolder' => '0',
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '1.0.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.4',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
 );
